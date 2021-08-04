8dce3e8498a5477ff499afdb4f8f8b2e7ea29e97
Sierra-Soto <sierra_soto@uhaul.com>
2021-08-04 15:55:02 -0700

    Rust
    Crash Course Rust

---
+++ src/print.rs
@@ -0,0 +1,21 @@
+pub fn run() {
+// print to console
+println!("Hello from the print.rs file");
+
+//Basic Formatting
+println!("{} is from {}", "Sierra", "AZ");
+
+//Positional Arguments
+println!("{0} is from {1} and {0} likes to {2}",
+"Sierra", "AZ", "code" );
+
+// Named Arguments (named paramaters)
+println!("{name} likes to play {activity}", 
+name = "Sally", activity = "piano");
+
+//placeholder traits
+println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10 );
+
+// Placeholder for debug trait
+println!("{:?}", (12, true, "hello"));
+}
