ba3b5cc50724cc47fd9cf03eb939be8e1db2d8fb
Sierra-Soto <sierra.sage@icloud.com>
2021-08-11 13:23:13 -0700

    types-rust
    types

--- src/vars.rs
+++ src/vars.rs
@@ -6,6 +6,16 @@
 
 pub fn run() {
 let name = "Sage";
+let mut age = 37;
+println!("My name is {} and I am {}", name, age);
+age = 38;
+println!("My name is {} and I am {}", name, age);
 
-println!("My name is {}", name);
+//Define constant
+const ID: i32 = 001;
+println!("ID: {}", ID);
+
+//Assign multiple variables at once
+let( my_name, my_age ) = ("Sage", 37);
+println!("{} is {}", my_name, my_age );
 }
