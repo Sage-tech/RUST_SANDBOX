a6687e7c4b104bfbac1933a704da57784f44ff80
Sierra-Soto <sierra.sage@icloud.com>
2021-08-27 14:40:39 -0700

    UPDATE
    RUST

--- src/types.rs
+++ src/types.rs
@@ -19,9 +19,18 @@ let x = 1;
 // default is "f64"  float
 let y = 2.5;
 // Add explicit type
-let y: i64 = 890809898889948;
+let z: i64 = 890809898889948;
 // Find max size 
 println!("Max i32: {}", std::i32::MAX);
 println!("Max i64: {}", std::i64::MAX);
+
+// Boolean
+let is_active = true;
+
+// Get boolean from expression
+let is_greater = 10 > 5;
+
+println!("{:?}", (x, y, z, is_active, is_greater));
+
 
 }
