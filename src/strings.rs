219b70f617ef757e013a3123636e26f101e80414
Sierra-Soto <sierra.sage@icloud.com>
2021-09-01 14:19:39 -0700

    rust
    strings start up

---
+++ src/strings.rs
@@ -0,0 +1,11 @@
+// Primitive str = Immutable fixed-length string somewhere in memory
+// String = Growable, heap-allocated data strucutre - Use when you ned to modify or own string data
+
+mod strings;
+
+fn main() {
+
+    //print::run();
+    //vars::run();
+    strings::run();
+}
--- src/types.rs
+++ src/types.rs
@@ -28,9 +28,13 @@ println!("Max i64: {}", std::i64::MAX);
 let is_active = true;
 
 // Get boolean from expression
-let is_greater = 10 > 5;
+let is_greater: bool = 10 < 5;
 
-println!("{:?}", (x, y, z, is_active, is_greater));
+let a1 = 'a';
+
+let face = '\u{1F600}';
+
+println!("{:?}", (x, y, z, is_active, is_greater, a1, face));
 
 
 }
